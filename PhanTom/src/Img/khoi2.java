40a72eb8bdced98e8c0c0a28a17428d2c6792424
khoi1233 <114672335+khoi1233@users.noreply.github.com>
2022-11-07 20:25:57 +0700

    Create khoi2.java
    khôi

---
+++ PhanTom/src/Img/khoi2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Img;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class khoi2 {
+    public static void main(String[] args) {
+        System.out.println("khôi");
+    }
+}
